7182246998490666743fccca5573bf59f79ee6af
Jellybean21 <david.j@codeur.online>
2019-01-29 09:11:58 +0100

    updated to fetch the modal

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,6 +1,7 @@
 var shareImageButton = document.querySelector('#share-image-button');
 var createPostArea = document.querySelector('#create-post');
 var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var sharedMomentsArea = document.querySelector('#shared-moments');
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
@@ -33,13 +34,17 @@ function createCard(){
   var cardWrapper = document.createElement('div');
   cardWrapper.classname = 'shared-moment-card mdl-card mdl-shadow--2dp';
   var cardTitle = document.createElement('div');
-  cardTitle.className = 'mdl-card_title';
+  cardTitle.className = 'mdl-card__title';
   cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
-  cardTitle.stylebackgroundSize = 'cover';
+  cardTitle.style.backgroundSize = 'cover';
   cardTitle.style.height = '180px';
   cardWrapper.appendChild(cardTitle);
+  var cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.className = 'mdl-card__title-text';
+  cardTitleTextElement.textContent = 'In San Francisco trip';
+  cardTitle.appendChild(cardTitleTextElement);
   var cardSupportingText = document.createElement ('div');
-  cardSupportingText.className = 'mdl-card_supporting-text';
+  cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = 'In San Francisco';
   cardSupportingText.style.textAlign = 'center';
   cardWrapper.appendChild(cardSupportingText);
