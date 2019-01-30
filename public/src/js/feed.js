d4a2ded6ae62846d876dea24d8cad84284349dbc
Jellybean21 <david.j@codeur.online>
2019-01-30 17:13:29 +0100

    update with caches strategys

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -21,8 +21,17 @@ function openCreatePostModal() {
     });
     defferedPrompt = null;
   }
+  //unregister a service worker bind on function openCreatePostModal()
+  /*if ('serviceWorker' in navigator){
+    navigator.serviceWorker.getRegistrations()
+    .then(function (registrations) {
+      for (var i = 0; i < registrations.length; i++){
+        registrations[i].unregister();
+      }
+    })
+  }
 }
-
+*/
 function closeCreatePostModal() {
   createPostArea.style.display = 'none';
 }
@@ -30,10 +39,25 @@ function closeCreatePostModal() {
 shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
-
+// Not in use , allow to save assets i cache on demand
+function onClickSavedButton(event){
+  console.log('button clicked');
+  if ('caches' in window){ // check if caches object exists
+    caches.open('user-requested')
+    .then(function(cache){
+      cache.add('https://httpbin.org/get');//request will be sent to this url
+      cache.add('/src/images/sf-boat.jpg');
+    });
+  }
+}
+function clearCards(){
+  while(sharedMomentsArea.hasChildNodes()){
+    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
+  }
+}
 function createCard(){
   var cardWrapper = document.createElement('div');
-  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp mrmlauto';
   var cardTitle = document.createElement('div');
   cardTitle.className = 'mdl-card__title';
   cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
@@ -49,15 +73,51 @@ function createCard(){
   cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = 'In San Francisco';
   cardSupportingText.style.textAlign = 'center';
+  //var cardSaveButton = document.createElement('button');
+  //cardSaveButton.textContent = 'Save';
+  //cardSaveButton.addEventListener('click', onClickSavedButton);
+  //cardSupportingText.appendChild(cardSaveButton);
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
 }
+let url = 'https://httpbin.org/post';
+let netWorkDataReceived = false;
 
-fetch('https://httpbin.org/get')
+
+fetch(url, {
+  method: 'POST',
+  headers: {
+    'Content-type': 'application/json',
+    'Accept': 'application/json',
+  },
+  body: JSON.stringify({
+    message: 'Some messages'
+  })
+})
 .then(function(res){
   return res.json();
 })
 .then(function(data){
+  netWorkDataReceived = true; // set true when we get the data from web, with that the only one to use the cache response
+  console.log('From web', data);// It's kind of a race , one could be faster than the other one
+  clearCards();
   createCard();
 })
+
+
+if ('caches' in window){
+  caches.match(url)
+  .then(function(response){
+    if (response) {
+      return response.json();
+    }
+  }).then(function(data){
+    console.log('From cache', data);
+    if (!netWorkDataReceived){
+      clearCards();
+      createCard();
+    }
+
+  });
+}
--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,33 @@
-let CACHE_STATIC_NAME = 'static-v4';
-let CACHE_DYNAMIC_NAME = 'dynamic-v2';
-
-
+let CACHE_STATIC_NAME = 'static-v24';
+let CACHE_DYNAMIC_NAME = 'dynamic-v12';
+let STATIC_FILES = [
+  '/',
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/promise.js',
+  '/src/js/fetch.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'];
+//function trimCache(cacheName, maxItems){// function to control how many items max we can cache , it delete the oldest.
+//  caches.open(cacheName)
+//    .then(function(cache){
+//      return cache.keys()
+//      .then(function(keys){
+//        if(keys.length > maxItems){
+//          cache.delete(keys[0])// delete the older items in keys array
+//            .then(trimCache(cacheName, maxItems));
+//        };
+//    })
+//
+//    });
+//}
 self.addEventListener('install', function(event){
   console.log('[Service Worker] Installing Service Worker...', event);
   //lets open the cache
@@ -9,21 +35,9 @@ self.addEventListener('install', function(event){
     caches.open(CACHE_STATIC_NAME)// static is the name of your cache , you can give a name you like
       .then(function(cache) {
         console.log('[Service Worker] Precaching app shell');
-        cache.addAll([
-          '/',
-          '/index.html',
-          '/src/js/app.js',
-          '/src/js/feed.js',
-          '/src/js/promise.js',
-          '/src/js/fetch.js',
-          '/src/js/material.min.js',
-          '/src/css/app.css',
-          '/src/css/feed.css',
-          '/src/images/main-image.jpg',
-          'https://fonts.googleapis.com/css?family=Roboto:400,700',
-          'https://fonts.googleapis.com/icon?family=Material+Icons',
-          'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-        ]);
+        cache.addAll(STATIC_FILES);
+
+
 
 
       }) //in the then block we execute a function and we passe it an argument by the opening method
@@ -53,7 +67,7 @@ self.addEventListener('activate', function(event){
 });
 //Last step we activate the Serice Worker
 //the following step allow us to cache dynamicaly.
-self.addEventListener('fetch', function(event){
+/*self.addEventListener('fetch', function(event){
    // fetch event will be triggered when by exemple the html page load something like assets like js files or css files ect... or when image is loading
   event.respondWith(
     caches.match(event.request) // caches.match allow us to call match to have a look at all our sub caches and see if we find given resource here.We match our request event.
@@ -68,12 +82,110 @@ self.addEventListener('fetch', function(event){
               cache.put(event.request.url, res.clone());//first argument we passe to put this in is the url identifier. the second argument is the response or rest , is the name here.
               return res;// this will allows us to continue with network request if we want to get something wich is not cached but get it from the cache
             })
-          }).catch(function(err){
-            console.log('error' + err);
+          }).catch(function(err){ // if it throw an error like other pages not cached , we are able to open the static cache and match it to serve the offline.html file we cached earlier.
+            return caches.open(CACHE_STATIC_NAME)
+            .then(function (cache){
+              return cache.match('/offline.html'); // return the value of cache.match
+            });
           });
         }
       })
 
     //request are our keys
   );
+});*/
+function isInArray(string, array){
+  let cachePath;
+  if(string.indexOf(self.origin) === 0){
+    console.log('matched', string);
+    cachePath = string.substring(self.origin.length);
+  }else{
+    cachePath = string;
+  }
+  return false;
+}
+//Cache then Network strategy , dynamic caching
+self.addEventListener('fetch', function(event){
+  let url = 'https://httpbin.org/get';//cache network strategie for this url
+  if(event.request.url.indexOf(url) > -1){//we can check if the event request url contains that url // > -1 mean we found and entry of 'https://httpbin.org/get' in event.request.url
+    event.respondWith(
+      caches.open(CACHE_DYNAMIC_NAME)
+        .then(function(cache){ //function the cache we opened as argument
+          return fetch(event.request)
+          .then(function (res){ // using the response wich it give to us
+            //trimCache(CACHE_DYNAMIC_NAME, 5);
+            cache.put(event.request, res.clone());
+            return res;
+          })
+        })
+    );
+    //let's check if we are loading one of the files we cached in static cache
+  }else if (isInArray(event.request.url, STATIC_FILES)){//(new RegExp('\\b' + STATIC_FILES.join('\\b|\\b') + '\\b').test(event.request.url))// if in the static file array that contains any of the url , the request url , contains any elements of the Regex in that array. It means basically if the url matches the regex separated words.
+    event.respondWith(
+      caches.match(event.request)// We respond with the cache only strategy
+    );
+  }else {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response){
+          if(response) {
+            return response;
+          }else{
+            return fetch(event.request)
+            .then(function(res){
+              return caches.open(CACHE_DYNAMIC_NAME)
+              .then(function(cache){
+                //trimCache(CACHE_DYNAMIC_NAME, 5)
+                cache.put(event.request.url, res.clone());
+                return res;
+              })
+            }).catch(function(err){
+              return caches.open(CACHE_STATIC_NAME)
+              .then(function (cache){
+                if ( event.request.headers.get('accept').includes('text/html')){// we call a get method to get a specific header. if the accept header includes text/html as answer
+                    return cache.match('/offline.html');                        //It means the incoming request accept html
+                }
+
+              });
+            });
+          }
+        })
+
+    );
+  }
+
 });
+
+/*self.addEventListener('fetch', function(event){
+
+  event.respondWith(
+    fetch(event.request)
+      .then(function (res){
+        return caches.open(CACHE_DYNAMIC_NAME)
+      .then(function(cache){
+        cache.put(event.request.url, res.clone());
+        return res;
+      })
+    })
+    .catch(function (error){
+    return caches.match(event.request)
+
+  })
+);
+});*/
+
+// Cash only strategy
+/*self.addEventListener('fetch', function(event){
+
+  event.respondWith(
+    caches.match(event.request)
+  );
+});*/
+
+//Network only strategy
+/*self.addEventListener('fetch', function(event){
+
+  event.respondWith(
+    fetch(event.request);
+  );
+});*/
